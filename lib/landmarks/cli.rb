11c342ed9df5cffae323d217b57283ffb1af559d
joshua richards <jermain119@gmail.com>
2019-11-04 22:24:19 +0000

    first commit

---
+++ lib/landmarks/cli.rb
@@ -0,0 +1,8 @@
+require "landmarks/cli/version"
+
+module Landmarks
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/landmarks/cli/version.rb
@@ -0,0 +1,5 @@
+module Landmarks
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
