4c2f8f252726e1f16f6e01db1d621d189499758d
joshua richards <jermain119@gmail.com>
2019-11-06 21:30:49 +0000

    update landmark app

--- lib/landmarks/scraper.rb
+++ lib/landmarks/scraper.rb
@@ -1,9 +1,11 @@
 class Scraper
-  
+  BASE_URL = "https://www.tripadvisor.com"
   def self.scrape_landmarks
-  url ="https://www.tripadvisor.com/Attractions-g187497-Activities-a_allAttractions.true-Barcelona_Catalonia.html"
+  url = BASE_URL + "/Attractions-g187497-Activities-a_allAttractions.true-Barcelona_Catalonia.html"
   #binding.pry  
         doc = Nokogiri::HTML(open(url))
+        #while loop 1o 
+        
         doc.css(".attraction_list .listing").each do |lm|
             landmark = Landmark.new
             landmark.name = lm.css('a')[1].text
@@ -13,8 +15,8 @@ class Scraper
   end
 
   def self.scrape_landmark_details(landmark)
-    
-    
+    url = BASE_URL + landmark.url
+    #scrape the url that was passed in
   end
   
 end 
