fc5182e1e44413a4419ddc2c4361099c318f7ff0
joshua richards <jermain119@gmail.com>
2019-11-07 18:44:01 +0000

    printing output from user selection

--- lib/landmarks/cli.rb
+++ lib/landmarks/cli.rb
@@ -8,19 +8,29 @@ class Cli
        print_landmarks
       puts " "
       puts " select the number of landmark to see further deatails or type 'exit' to exit"  
-  #     input = gets.strip.downcase
-  #     while input != 'exit' do 
-  #       landmark = Landmark.all[input.to_i - 1]
-  #     end
+      input = gets.strip.downcase
+      while input != 'exit' do 
+        landmark = Landmark.all[input.to_i - 1]
+        Scraper.scrape_landmark_details(landmark) 
+        print_landmark(landmark)
+        puts "  "
+        puts "would you like to see another landmark?"
+        puts " if so make a selection from the number list type 'exit' to exit "
+         input = gets.strip.downcase
+      end
+      
+      puts "good bye"
    end
-    # def menu
+    def print_landmark(landmark)
+      puts "#{landmark.name}"
+      puts " Hours_of_operation: #{landmark.hours_of_operation}"
   
-    # end
+    end
 
       def print_landmarks
       Landmark.all.each.with_index(1) do |landmark, index|
       
-      puts "#{index}. #{landmark.name}"
+      puts "#{index}.#{landmark.name}"
   
        end
     end 
