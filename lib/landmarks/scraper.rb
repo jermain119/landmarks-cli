9acbe040e45301158904b15ddedf341877f8a420
joshua richards <jermain119@gmail.com>
2019-11-07 20:57:04 +0000

    prints user info

--- lib/landmarks/scraper.rb
+++ lib/landmarks/scraper.rb
@@ -6,22 +6,23 @@ class Scraper
         doc = Nokogiri::HTML(open(url))
         #while loop 1o 
         
-        doc.css(".attraction_list .listing").each do |lm|
+        doc.css(".attraction_list .listing")[0..10].each do |lm|
+    
             landmark = Landmark.new
             landmark.name = lm.css('a')[1].text
             landmark.url = lm.css('a')[1].attribute("href").value
         end
-
+  
   end
 
   def self.scrape_landmark_details(landmark)
     url = BASE_URL + landmark.url
     doc = Nokogiri::HTML(open(url))
-    landmark.about = doc.css("div.attractions-attraction-detail-about-card-AttractionDetailAboutCard__section--1_Efg")[1].text
-    landmark.hours_of_operation = doc.css("div.attractions-attraction-detail-about-card-AttractionDetailAboutCard__section--1_Efg")[3].text
     
+    landmark.about = doc.css("div.attractions-attraction-detail-about-card-AttractionDetailAboutCard__section--1_Efg")[1].css("a").text 
+    # landmark.hours_of_operation = doc.css("div.attractions-attraction-detail-about-card-AttractionDetailAboutCard__section--1_Efg")[3].text
     #scrape the url that was passed in
   end
-  
+
 end 
     
