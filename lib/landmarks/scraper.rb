a652fd6e3dd49fcf24ab3f9363a3fb3b92c53273
joshua richards <jermain119@gmail.com>
2019-11-09 16:04:50 +0000

    scope line up

--- lib/landmarks/scraper.rb
+++ lib/landmarks/scraper.rb
@@ -3,16 +3,16 @@ class Scraper
   BASE_URL = "https://www.tripadvisor.com"
   def self.scrape_landmarks
   url = BASE_URL + "/Attractions-g187497-Activities-a_allAttractions.true-Barcelona_Catalonia.html"
-  #binding.pry  
-        doc = Nokogiri::HTML(open(url))
-        #while loop 1o 
+ 
+  doc = Nokogiri::HTML(open(url))
+     
         
-        doc.css(".attraction_list .listing")[0..10].each do |lm|
+    doc.css(".attraction_list .listing")[0..10].each do |lm|
     
-            landmark = Landmark.new(name, url)
-            landmark.name = lm.css('a')[1].text
-            landmark.url = lm.css('a')[1].attribute("href").value
-        end
+     landmark = Landmark.new(name, url)
+      landmark.name = lm.css('a')[1].text
+      landmark.url = lm.css('a')[1].attribute("href").value
+    end
   
   end
 
@@ -23,9 +23,9 @@ class Scraper
     landmark.about = scraped_details
     
     if scraped_details
-      landmark.about = scraped_details.text 
-    else
-      landmark.about = "Sorry, no details found"
+        landmark.about = scraped_details.text 
+      else
+        landmark.about = "Sorry, no details found"
     end
     # landmark.hours_of_operation = doc.css("div.attractions-attraction-detail-about-card-AttractionDetailAboutCard__section--1_Efg")[3].text
     #scrape the url that was passed in
